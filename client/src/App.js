c498e98798a962e26979b689716b07aa202f2f88
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-10-12 17:28:18 +0900

    Add backend and changed package.json to correct routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 
@@ -21,7 +21,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
---
+++ server/server.js
@@ -0,0 +1,1 @@
+// backend code goes here
