1b2fd7f089002afb81de16186ff1f068d1a68696
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-10-17 18:57:35 +0630

    added backend starter code

--- server/server.js
+++ server/server.js
@@ -1,1 +1,11 @@
-// backend code goes here
+const express = require("express");
+const app = express();
+
+app.get("/", (request, response) => {
+  response.send("<h1>This is our backend</h1>");
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
