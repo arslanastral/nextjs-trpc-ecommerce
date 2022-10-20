5bcc6de736420c0b4e9f4fe1ac6bca675cf1bcf2
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-10-20 08:32:50 +0630

    added home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Login from "./pages/authen/Login/";
 import Signup from "./pages/authen/Signup";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
