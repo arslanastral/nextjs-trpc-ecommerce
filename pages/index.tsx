b46679c2c47750c3d4c284d5c8a8859ebd7b4436
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-10-27 18:12:07 +0800

    Edit bg of the products

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ const Home: PageWithLayout = () => {
   }, []);
 
   return (
-    <div className="flex flex-col p-6 items-center justify-center 2xl:justify-around min-h-screen min-w-full bg-black text-white">
+    <div className="flex flex-col p-6 items-center justify-center 2xl:justify-around min-h-screen min-w-full text-white">
       <Head>
         <title>Zavy</title>
         <meta name="description" content="An ecommerce store" />
