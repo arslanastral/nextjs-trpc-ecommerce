160851946b7e3f24049850350d39a5cc303a3611
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-11-01 19:03:13 +0800

    Add desktop compatablity

--- src/pages/products/[...id].tsx
+++ src/pages/products/[...id].tsx
@@ -39,69 +39,73 @@ function Products() {
       <ControlBar />
       <Logo />
       {product && (
-        <div className="flex flex-col w-full m-3 lg:flex-row">
-          <div>
-            <Image
-              src={product.image}
-              alt={product.description}
-              className="w-full h-60 object-contain"
-              width={250}
-              height={250}
-            />
-          </div>
-          <div className="my-3 flex flex-col">
-            <div className="w-full flex justify-between my-4">
-              <p className="mr-auto">Seller name</p>
-              <p className="mr-1">Listed on: 11-1-2022</p>
+        <div className="flex items-center justify-center">
+          <div className="flex flex-col w-full m-3 lg:flex-row lg:max-w-5xl xl:max-w-7xl">
+            <div className="lg:mr-10">
+              <Image
+                src={product.image}
+                alt={product.description}
+                className="w-full h-60 object-contain lg:gap-2 lg:h-auto"
+                width={250}
+                height={250}
+              />
             </div>
-            <h1 className="text-xl font-bold">{product.title}</h1>
-            <p className="capitalize text-zinc-500">{product.category}</p>
-            <p className="my-10 order-4 md:order-5">
-              {!isMaxCharacters && <>{product.description}</>}
-              {!clickedReadMore && isMaxCharacters && (
-                <>
-                  {product.description.substring(0, MAX_CHARACTERS) + '...'}{' '}
-                  <button className="text-blue-500" onClick={() => setClickedReadMore(true)}>
-                    Read More
-                  </button>
-                </>
-              )}
-              {clickedReadMore && isMaxCharacters && (
-                <>
-                  {product.description + ' '}
-                  <button className="text-blue-500" onClick={() => setClickedReadMore(false)}>
-                    Read Less
-                  </button>
-                </>
-              )}
-            </p>
-            <div
-              className={`${
-                clickedReadMore && 'shadow-[0px_0px_12px_rgba(0,0,0,0.12)]'
-              } mr-1 order-5 md:order-4 py-3`}
-            >
-              <p className="font-bold text-2xl text-right mb-3">$ {product.price}</p>
-              <div className="flex">
-                <div className="w-[40%] flex">
-                  <div
-                    className="p-3 px-5 md:px-8 bg-zinc-100 rounded-l-lg border border-zinc-200 text-zinc-500"
-                    onClick={addOrderCount}
-                  >
-                    +
+            <div className="my-3 flex flex-col">
+              <div className="w-full flex justify-between my-4">
+                <p className="mr-auto">Seller name</p>
+                <p className="mr-1">Listed on: 11-1-2022</p>
+              </div>
+              <h1 className="text-xl font-bold text-zinc-600">{product.title}</h1>
+              <p className="capitalize text-zinc-500">{product.category}</p>
+              <p className="my-10 order-4 lg:order-5 lg:max-w-3xl">
+                {!isMaxCharacters && <>{product.description}</>}
+                {!clickedReadMore && isMaxCharacters && (
+                  <>
+                    {product.description.substring(0, MAX_CHARACTERS) + '...'}{' '}
+                    <button className="text-blue-500" onClick={() => setClickedReadMore(true)}>
+                      Read More
+                    </button>
+                  </>
+                )}
+                {clickedReadMore && isMaxCharacters && (
+                  <>
+                    {product.description + ' '}
+                    <button className="text-blue-500" onClick={() => setClickedReadMore(false)}>
+                      Read Less
+                    </button>
+                  </>
+                )}
+              </p>
+              <div
+                className={`${
+                  clickedReadMore && 'shadow-[0px_0px_12px_rgba(0,0,0,0.12)] md:shadow-none'
+                } mr-1 order-5 md:order-4 py-3`}
+              >
+                <p className="font-extrabold text-4xl text-right mb-3 lg:mb-10 text-zinc-600">
+                  $ {product.price}
+                </p>
+                <div className="flex">
+                  <div className="w-[40%] flex">
+                    <div
+                      className="p-3 px-5 md:px-8 bg-zinc-100 rounded-l-lg border border-zinc-200 text-zinc-500"
+                      onClick={subractOrderCount}
+                    >
+                      -
+                    </div>
+                    <div className="p-3 px-5 md:px-10 lg:px-10 border border-zinc-200 text-zinc-500">
+                      {orderCount}
+                    </div>
+                    <div
+                      className="p-3 px-5 md:px-8 bg-zinc-100 rounded-r-lg border border-zinc-200 text-zinc-500"
+                      onClick={addOrderCount}
+                    >
+                      +
+                    </div>
                   </div>
-                  <div className="p-3 px-5 md:px-10 lg:px-10 border border-zinc-200 text-zinc-500">
-                    {orderCount}
-                  </div>
-                  <div
-                    className="p-3 px-5 md:px-8 bg-zinc-100 rounded-r-lg border border-zinc-200 text-zinc-500"
-                    onClick={subractOrderCount}
-                  >
-                    -
-                  </div>
+                  <Button className="w-[60%] bg-brown-600" size="lg">
+                    Add to Cart
+                  </Button>
                 </div>
-                <Button className="w-[60%] bg-brown-600" size="lg">
-                  Add to Cart
-                </Button>
               </div>
             </div>
           </div>
