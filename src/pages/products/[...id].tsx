4fd811530967db04e40c3d2ab847f5f9b0c31bd4
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-11-01 19:19:12 +0800

    Add information all about navbar being gone

--- src/pages/products/[...id].tsx
+++ src/pages/products/[...id].tsx
@@ -39,7 +39,11 @@ function Products() {
       <ControlBar />
       <Logo />
       {product && (
-        <div className="flex items-center justify-center">
+        <div className="flex flex-col items-center justify-center">
+          <div className="text-yellow-600 bg-yellow-100 border border-yellow-200 rounded-xl p-4 text-center">
+            If you see this warning then, that means that the navbar isn&apos;t responsive yet or
+            the creator hasn&apos;t been updated about it yet
+          </div>
           <div className="flex flex-col w-full m-3 lg:flex-row lg:max-w-5xl xl:max-w-7xl">
             <div className="lg:mr-10">
               <Image
