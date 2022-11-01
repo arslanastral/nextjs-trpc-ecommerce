908fc3d91612c9caa5aa957f94f9d2684185ef5b
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-11-01 15:06:01 +0800

    Add description, seller, and date

--- src/pages/products/[...id].tsx
+++ src/pages/products/[...id].tsx
@@ -6,8 +6,11 @@ import { useRouter } from 'next/router';
 import Image from 'next/image';
 function Products() {
   const [product, setProduct] = useState<Product>();
+  const [clickedReadMore, setClickedReadMore] = useState(false);
   const router = useRouter();
   const { id } = router.query;
+  const MAX_CHARACTERS = 135;
+  let isMaxCharacters = false;
   useEffect(() => {
     console.log(id);
     if (id) {
@@ -16,6 +19,9 @@ function Products() {
         .then((data) => setProduct(data));
     }
   }, [id]);
+  if (product?.description && product?.description.length > MAX_CHARACTERS) {
+    isMaxCharacters = true;
+  }
   console.log(product);
   return (
     <>
@@ -32,7 +38,31 @@ function Products() {
           />
         </div>
         <div className="my-3">
+          <div className="w-full flex justify-between my-4">
+            <p className="mr-auto">Seller name</p>
+            <p className="mr-1">Listed on: 11-1-2022</p>
+          </div>
           <h1 className="text-xl font-bold">{product?.title}</h1>
+          <p className="capitalize text-zinc-500">{product?.category}</p>
+          <p className="my-10">
+            {!isMaxCharacters && <>{product?.description}</>}
+            {!clickedReadMore && isMaxCharacters && (
+              <>
+                {product?.description.substring(0, MAX_CHARACTERS) + '...'}{' '}
+                <button className="text-blue-500" onClick={() => setClickedReadMore(true)}>
+                  Read More
+                </button>
+              </>
+            )}
+            {clickedReadMore && isMaxCharacters && (
+              <>
+                {product?.description + ' '}
+                <button className="text-blue-500" onClick={() => setClickedReadMore(false)}>
+                  Read Less
+                </button>
+              </>
+            )}
+          </p>
         </div>
       </div>
     </>
