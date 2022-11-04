5cea12cc83165d794da872c576acd036b135c99f
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-11-04 22:38:26 +0800

    added address input schema

--- src/server/schema.ts
+++ src/server/schema.ts
@@ -1,5 +1,17 @@
 import { z } from 'zod';
 
+export const addressInput = z.object({
+  isDefault: z.boolean().optional(),
+  unitNumber: z.string().optional(),
+  StreetNumber: z.string().optional(),
+  addressLine1: z.string(),
+  addressLine2: z.string().optional(),
+  city: z.string(),
+  postalCode: z.string(),
+  region: z.string(),
+  country: z.string()
+});
+
 export const sellerInfoInput = z.object({
   storeName: z.string().optional(),
   storeEmail: z.string().optional()
