d1224b9af2fcaf452b71938522bd56b58523c13c
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-11-04 23:49:58 +0800

    added address loading skeleton

--- src/lib/components/Dashboard/Buyer/Address.tsx
+++ src/lib/components/Dashboard/Buyer/Address.tsx
@@ -1,52 +1,41 @@
-import { Card, Text, Badge, Button, Group } from '@mantine/core';
+import { Button } from '@mantine/core';
 import { IconPlus } from '@tabler/icons';
+import { trpc } from '@/utils/trpc';
+import { Skeleton } from '@mantine/core';
+import AddressCard from './AddressCard';
 
-function Address({}) {
+function Address() {
+  const addresses = trpc.address.list.useQuery();
+
   return (
     <div className="p-8">
       <h2 className="font-semibold text-4xl mb-7">Address Book</h2>
 
       <div className="flex gap-4 items-center flex-wrap">
-        <div className="w-64 h-56 border-dashed border-2 border-brown-200 rounded-xl flex items-center justify-center text-brown-600">
-          <Button
-            leftIcon={<IconPlus size="15px" />}
-            variant="subtle"
-            mt="md"
-            radius="md"
-            // className="w-4/5 "
-          >
-            Add Address
-          </Button>
-        </div>
-
-        <div className="w-64">
-          <Card p="lg" radius="md" withBorder>
-            <Group position="apart" mt="md" mb="xs">
-              <Text weight={500}>119 New York</Text>
-              <Badge variant="light">default</Badge>
-            </Group>
-
-            <Text size="sm" color="dimmed">
-              469 Linden Drive Twin Falls
-            </Text>
-
-            <Text size="sm" color="dimmed">
-              10512
-            </Text>
-
-            <Text size="sm" color="dimmed">
-              New York
-            </Text>
-
-            <Text size="sm" color="dimmed">
-              United States
-            </Text>
-
-            <Button variant="light" fullWidth mt="md" radius="md">
-              Edit
+        {!addresses.isLoading && (
+          <div className="w-64 h-56 border-dashed border-2 border-brown-200 rounded-xl flex items-center justify-center text-brown-600">
+            <Button
+              disabled={true}
+              // onClick={createAddress}
+              leftIcon={<IconPlus size="15px" />}
+              variant="subtle"
+              mt="md"
+              radius="md"
+              // className="w-4/5 "
+            >
+              Add Address
             </Button>
-          </Card>
-        </div>
+          </div>
+        )}
+
+        {!addresses.data && (
+          <div className="w-64 mt-7">
+            <Skeleton height={10} mt={7} width="40%" radius="xl" />
+            <Skeleton height={10} mt={7} radius="xl" />
+            <Skeleton height={10} mt={7} radius="xl" />
+            <Skeleton height={10} mt={7} width="70%" radius="xl" />
+          </div>
+        )}
       </div>
     </div>
   );
