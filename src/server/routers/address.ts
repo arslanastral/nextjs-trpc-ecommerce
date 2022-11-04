54609ff23a8baf0d94644f14286be0cf112d2ebc
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-11-04 22:38:59 +0800

    created address creation procedure

--- src/server/routers/address.ts
+++ src/server/routers/address.ts
@@ -1,7 +1,30 @@
 import { router, protectedProcedure } from '../trpc';
 import { getBuyerId } from '@/server/functions/identity';
+import { addressInput } from '../schema';
 
 export const addressRouter = router({
+  create: protectedProcedure.input(addressInput).mutation(async ({ input, ctx }) => {
+    let buyerId = await getBuyerId(ctx);
+    if (!buyerId) return null;
+
+    let newAddress = await ctx.prisma.address.create({
+      data: {
+        isDefault: input.isDefault,
+        unitNumber: input.unitNumber,
+        StreetNumber: input.StreetNumber,
+        addressLine1: input.addressLine1,
+        addressLine2: input.addressLine2,
+        city: input.city,
+        postalCode: input.postalCode,
+        region: input.region,
+        country: input.country,
+        buyerId: buyerId
+      }
+    });
+
+    return newAddress;
+  }),
+
   list: protectedProcedure.query(async ({ ctx }) => {
     let buyerId = await getBuyerId(ctx);
     if (!buyerId) return null;
