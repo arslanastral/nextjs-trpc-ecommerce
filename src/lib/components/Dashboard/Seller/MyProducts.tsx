39cfcdcc962abd5c440cedc390b23e8dad9756aa
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-11-06 21:36:25 +0800

    added placeholder cards

--- src/lib/components/Dashboard/Seller/MyProducts.tsx
+++ src/lib/components/Dashboard/Seller/MyProducts.tsx
@@ -1,14 +1,34 @@
-import { Button } from '@mantine/core';
+import { Button, Card, Group, Text, Image, Badge } from '@mantine/core';
 import { IconPlus } from '@tabler/icons';
+import ProductCard from './ProductCard';
 
 function MyProducts() {
   return (
     <div className="p-8">
       <h2 className="font-semibold text-4xl mb-7">My Products</h2>
-      <div className="w-64 h-72 border-dashed border-2 border-brown-200 rounded-xl flex items-center justify-center text-brown-600">
-        <Button leftIcon={<IconPlus size="15px" />} variant="outline" mt="md" radius="md">
-          New Product
-        </Button>
+      <div className="flex gap-8 items-center flex-wrap">
+        <div className="w-[337px] h-[583px] border-dashed border-2 border-brown-200 rounded-xl flex items-center justify-center text-brown-600">
+          <Button leftIcon={<IconPlus size="15px" />} variant="outline" mt="md" radius="md">
+            New Product
+          </Button>
+        </div>
+
+        <ProductCard
+          title="Blue dress with silk cotten and smooth"
+          description="Blue floral dress with beach vibes"
+          price="20"
+          status="sold out"
+          image="https://images.unsplash.com/photo-1539008835657-9e8e9680c956?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+        />
+
+        <ProductCard
+          title="Blue dress with silk cotten and smooth"
+          description="Blue floral dress with beach vibes"
+          price="20"
+          status="sold out"
+          badge="new"
+          image="https://images.unsplash.com/photo-1612722432474-b971cdcea546?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80"
+        />
       </div>
     </div>
   );
