871bd662dc474bb848105aeab77bfa87bfef164d
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-11-06 21:32:37 +0800

    implemented seller product card

--- src/lib/components/Dashboard/Seller/ProductCard.tsx
+++ src/lib/components/Dashboard/Seller/ProductCard.tsx
@@ -1,5 +1,43 @@
+import { Button, Card, Group, Text, Badge, AspectRatio } from '@mantine/core';
+import Image from 'next/image';
+
 function ProductCard() {
-  return <div>Product Card</div>;
+  return (
+    <Card p="lg" radius="md" withBorder className="relative">
+      <Card.Section>
+        <Badge className="absolute top-2 left-3 z-40 bg-black text-white">new</Badge>
+        <AspectRatio ratio={337 / 393} sx={{ maxWidth: '100%' }} className="relative">
+          <Image fill={true} src={image} alt="Norway" />
+        </AspectRatio>
+      </Card.Section>
+      <Group position="apart" mt="md" mb="xs">
+        <Text
+          className=" whitespace-nowrap"
+          weight={500}
+          sx={{ textOverflow: 'ellipsis', overflow: 'hidden' }}
+        >
+          {title}
+        </Text>
+      </Group>
+
+      <Text size="sm" color="dimmed">
+        {description}
+      </Text>
+
+      <Group position="apart" mt="md" mb="xs">
+        <Text size={20} weight={500}>
+          US ${price}
+        </Text>
+        <Badge variant="light" color="red">
+          {status}
+        </Badge>
+      </Group>
+
+      <Button fullWidth mt="md" radius="md">
+        Edit
+      </Button>
+    </Card>
+  );
 }
 
 export default ProductCard;
