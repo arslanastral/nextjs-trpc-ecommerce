f30ae038e7a87d5c68ed8f602a4f925257b69a56
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-11-06 13:40:43 +0800

    created new product button

--- src/lib/components/Dashboard/Seller/MyProducts.tsx
+++ src/lib/components/Dashboard/Seller/MyProducts.tsx
@@ -1,5 +1,17 @@
+import { Button } from '@mantine/core';
+import { IconPlus } from '@tabler/icons';
+
 function MyProducts() {
-  return <div>My Products</div>;
+  return (
+    <div className="p-8">
+      <h2 className="font-semibold text-4xl mb-7">My Products</h2>
+      <div className="w-64 h-72 border-dashed border-2 border-brown-200 rounded-xl flex items-center justify-center text-brown-600">
+        <Button leftIcon={<IconPlus size="15px" />} variant="outline" mt="md" radius="md">
+          New Product
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default MyProducts;
