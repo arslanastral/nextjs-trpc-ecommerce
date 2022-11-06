e78700b8a884975cd32e61185eeb492ce044b4e6
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-11-06 22:16:12 +0800

    added product modal props with types

--- src/lib/components/Dashboard/Seller/ProductModal.tsx
+++ src/lib/components/Dashboard/Seller/ProductModal.tsx
@@ -1,6 +1,12 @@
 import { Modal, Button, Input, Grid, Checkbox, LoadingOverlay, Alert } from '@mantine/core';
 
-function ProductModal() {
+type ProductModalProps = {
+  opened: boolean;
+  setOpened: (state: boolean) => void;
+  data?: {};
+};
+
+function ProductModal({ opened, setOpened, data }: ProductModalProps) {
   return (
     <Modal opened={opened} onClose={() => setOpened(false)} title="Add New Product">
       {/* Modal content */}
