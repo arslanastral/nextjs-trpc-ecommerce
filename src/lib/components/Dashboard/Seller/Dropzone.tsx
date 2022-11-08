d3e9976c89f5e6cdb9d3d75cf17e61d35a69eb82
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-11-08 21:30:09 +0800

    removed comment

--- src/lib/components/Dashboard/Seller/Dropzone.tsx
+++ src/lib/components/Dashboard/Seller/Dropzone.tsx
@@ -41,14 +41,6 @@ export function DropzoneButton({ onDrop }: DropZoneProp) {
     onDrop(imageDataUrl);
   };
 
-  //   function readFile(file: File) {
-  //     return new Promise((resolve) => {
-  //       const reader = new FileReader();
-  //       reader.addEventListener('load', () => resolve(reader.result), false);
-  //       reader.readAsDataURL(file);
-  //     });
-  //   }
-
   return (
     <div className={classes.wrapper}>
       <Dropzone
