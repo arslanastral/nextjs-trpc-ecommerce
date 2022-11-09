dbc23d1bccc48674b373f564f27f4ad1fb0921aa
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-11-10 00:27:52 +0800

    implement product creation

--- src/server/routers/product.ts
+++ src/server/routers/product.ts
@@ -1,11 +1,33 @@
 import { router, protectedProcedure } from '../trpc';
 import { getBuyerId, getSellerId } from '@/server/functions/identity';
 import { productInput, productInputWithId } from '../schema';
+import { uploadToCloudinary } from '../functions/image';
 
 export const productRouter = router({
   create: protectedProcedure.input(productInput).mutation(async ({ input, ctx }) => {
-    // let product = await getBuyerId(ctx);
-    // return product;
+    let sellerId = await getSellerId(ctx);
+    if (!sellerId) return null;
+
+    let imageId = await uploadToCloudinary(input.image);
+    if (!imageId) return null;
+
+    let priceInCents = (input.price * 100).toString();
+    let itemCat = +input.category <= 7 ? +input.category : 7; //assign a known category
+    let product = await ctx.prisma.product.create({
+      data: {
+        description: input.description,
+        priceInCents: priceInCents,
+        image: imageId,
+        title: input.title,
+        category: {
+          connect: {
+            id: itemCat
+          }
+        },
+        sellerId: sellerId
+      }
+    });
+    return product;
   }),
 
   read: protectedProcedure.query(async ({ ctx }) => {}),
