57323bf61d5f21d36384bfda52bc5f573c75fa98
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-11-12 18:07:11 +0800

    setup product page query

--- src/pages/products/[...id].tsx
+++ src/pages/products/[...id].tsx
@@ -1,27 +1,36 @@
-import { useState, useEffect } from 'react';
-import { Product } from '@/lib/types/product';
 import { useRouter } from 'next/router';
 import Image from 'next/image';
 import { Button } from '@mantine/core';
 import { trpc } from '@/utils/trpc';
 import Layout from '@/lib/components/Layouts/Layout';
+import NextError from 'next/error';
 
 function Products() {
-  const router = useRouter();
-  const id = (router.query.id as string) ?? '';
-  const productData = trpc.product.sellableProductById.useQuery({ id: id });
+  const id = (useRouter().query.id as string) ?? '';
+
+  let product = trpc.product.sellableProductById.useQuery({ id: id[0] });
+
+  if (product.error) {
+    return (
+      <NextError title={product.error.message} statusCode={product.error.data?.httpStatus ?? 500} />
+    );
+  }
+
+  if (product.status !== 'success') {
+    return <>Loading...</>;
+  }
 
   return (
     <Layout>
-      {product && (
+      {product?.data && (
         <div className="flex flex-col w-full m-3 px-4 lg:flex-row lg:max-w-5xl xl:max-w-7xl ml-auto">
           <div className="lg:mr-10">
             <Image
-              src={product.image}
-              alt={product.description}
+              width={350}
+              height={450}
+              src={`https://res.cloudinary.com/dv9wpbflv/image/upload/v${product.data.image}.jpg`}
               className="w-full h-60 object-contain lg:gap-2 lg:h-auto"
-              width={250}
-              height={250}
+              alt="Product Image"
             />
           </div>
           <div className="my-3 flex flex-col">
@@ -29,12 +38,12 @@ function Products() {
               <p className="mr-auto">Seller name</p>
               <p className="mr-1">Listed on: 11-1-2022</p>
             </div>
-            <h1 className="text-xl font-bold text-zinc-600 max-w-3xl">{product.title}</h1>
-            <p className="capitalize text-zinc-500">{product.category}</p>
-            <p className="my-10 order-4 lg:order-5 lg:max-w-3xl">{productData}</p>
+            <h1 className="text-xl font-bold text-zinc-600 max-w-3xl">{product.data.title}</h1>
+            <p className="capitalize text-zinc-500">{product.data.category[0].name}</p>
+            <p className="my-10 order-4 lg:order-5 lg:max-w-3xl">{product.data.description}</p>
             <div className="mr-1 order-5 md:order-4 py-3">
               <p className="font-extrabold text-4xl text-right mb-5 lg:mb-10 text-zinc-600">
-                $ {product.price}
+                $ {(+product.data.priceInCents / 100).toString()}
               </p>
               <div className="flex">
                 <div className="w-[40%] flex">
@@ -42,7 +51,7 @@ function Products() {
                     -
                   </div>
                   <div className="p-3 px-5 md:px-8 lg:px-10 border border-zinc-200 text-zinc-500">
-                    {orderCount}
+                    {'10'}
                   </div>
                   <div className="p-3 px-5 md:px-6 bg-zinc-100 rounded-r-lg border border-zinc-200 text-zinc-500">
                     +
