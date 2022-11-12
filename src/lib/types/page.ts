e911133d700b6d3e80c36767c5c0ea9353319787
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-11-12 18:57:46 +0800

    updated types

--- src/lib/types/page.ts
+++ src/lib/types/page.ts
@@ -1,6 +1,9 @@
 import { ReactElement, ReactNode } from 'react';
 import { NextPage } from 'next';
 
-export type PageWithLayout = NextPage & {
-  getLayout: (page: ReactElement) => ReactNode;
+export type PageWithLayout<TProps = Record<string, unknown>, TInitialProps = TProps> = NextPage<
+  TProps,
+  TInitialProps
+> & {
+  getLayout?: (page: ReactElement) => ReactNode;
 };
