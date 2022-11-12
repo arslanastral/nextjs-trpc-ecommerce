c8f79f836517cb436afe129b5e73fe3482f4977e
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-11-13 00:44:26 +0800

    updated product page styling

--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -1,22 +1,13 @@
-import { useRouter } from 'next/router';
+import { Button, Spoiler, Avatar, AspectRatio, Text, Group } from '@mantine/core';
 import Image from 'next/image';
 import { ReactElement } from 'react';
-import { Button } from '@mantine/core';
 import { trpc } from '@/utils/trpc';
 import Layout from '@/lib/components/Layouts/Layout';
 import NextError from 'next/error';
 import { PageWithLayout } from '@/lib/types/page';
 import { GetServerSideProps } from 'next';
-
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const { id } = ctx.query;
-
-  return {
-    props: {
-      id
-    }
-  };
-};
+import { QuantityInput } from '@/lib/components/ProductPage/QuantityInput';
+import { IconShoppingCart } from '@tabler/icons';
 
 type PageProps = {
   id: string;
@@ -38,42 +29,64 @@ const ProductPage: PageWithLayout<PageProps> = ({ id }) => {
   return (
     <>
       {product?.data && (
-        <div className="flex flex-col w-full m-3 px-4 lg:flex-row lg:max-w-5xl xl:max-w-7xl ml-auto">
-          <div className="lg:mr-10">
-            <Image
-              width={350}
-              height={450}
-              src={`https://res.cloudinary.com/dv9wpbflv/image/upload/v${product.data.image}.jpg`}
-              className="w-full h-60 object-contain lg:gap-2 lg:h-auto"
-              alt="Product Image"
-            />
-          </div>
-          <div className="my-3 flex flex-col">
-            <div className="w-full flex justify-between my-4">
-              <p className="mr-auto">Seller name</p>
-              <p className="mr-1">Listed on: 11-1-2022</p>
+        <div className="lg:p-6 flex justify-center w-full min-h-[600px]">
+          <div className="flex flex-col justify-center lg:flex-row bg-white gap-6 max-w-[1300px]">
+            <div className="w-full md:w-3/5 lg:w-[450px] mx-auto">
+              <AspectRatio ratio={337 / 393} sx={{ maxWidth: '100%', minHeight: '100%' }}>
+                <Image
+                  fill={true}
+                  src={`https://res.cloudinary.com/dv9wpbflv/image/upload/v${product.data.image}.jpg`}
+                  alt="Product Image"
+                />
+              </AspectRatio>
             </div>
-            <h1 className="text-xl font-bold text-zinc-600 max-w-3xl">{product.data.title}</h1>
-            <p className="capitalize text-zinc-500">{product.data.category[0].name}</p>
-            <p className="my-10 order-4 lg:order-5 lg:max-w-3xl">{product.data.description}</p>
-            <div className="mr-1 order-5 md:order-4 py-3">
-              <p className="font-extrabold text-4xl text-right mb-5 lg:mb-10 text-zinc-600">
-                $ {(+product.data.priceInCents / 100).toString()}
-              </p>
-              <div className="flex">
-                <div className="w-[40%] flex">
-                  <div className="p-3 px-5 md:px-6 bg-zinc-100 rounded-l-lg border border-zinc-200 text-zinc-500">
-                    -
-                  </div>
-                  <div className="p-3 px-5 md:px-8 lg:px-10 border border-zinc-200 text-zinc-500">
-                    {'10'}
-                  </div>
-                  <div className="p-3 px-5 md:px-6 bg-zinc-100 rounded-r-lg border border-zinc-200 text-zinc-500">
-                    +
-                  </div>
-                </div>
-                <Button className="w-[60%] bg-brown-600" size="lg">
-                  Add to Cart
+
+            <div className="p-2 flex flex-1 flex-col justify-between">
+              <div className="p-5">
+                <Text transform="uppercase" color="dimmed" weight={700} size="xs">
+                  {product.data.category[0].name}
+                </Text>
+                <Text className="text-3xl" mt="xs" mb="md">
+                  {product.data.title}
+                </Text>
+              </div>
+
+              <div className="p-5">
+                <Text className="text-xl text-brown-500" mt="xs" mb="md" weight={300}>
+                  <Spoiler maxHeight={90} showLabel="Read More" hideLabel="Hide">
+                    {product.data.description}. We work with monitoring programs to guarantee
+                    compliance with the health, safety, and quality standards for our products. The
+                    Green to Wear 2.0 standard aims to minimize the environmental impact of textile
+                    manufacturing. To do this, we have developed Inditex’s The List program which
+                    helps us guarantee both the purity of production processes and the health and
+                    safety of our garments.
+                  </Spoiler>
+                </Text>
+              </div>
+
+              <div className="p-5 flex">
+                <Text className="text-4xl" mt="xs" mb="md">
+                  $ {(+product.data.priceInCents / 100).toString()}
+                </Text>
+              </div>
+
+              <div className=" gap-3 p-5 items-center justify-between hidden lg:flex">
+                <QuantityInput />
+                <Button fullWidth className="h-[50px] max-w-[20rem] text-xl font-light" radius="sm">
+                  Add To Cart
+                </Button>
+              </div>
+
+              <div className="fixed flex gap-3 p-5 w-full items-center justify-between lg:hidden bg-white shadow bottom-0 left-0 right-0">
+                <Text className="text-4xl">${(+product.data.priceInCents / 100).toString()}</Text>
+                <QuantityInput />
+                <Button
+                  leftIcon={<IconShoppingCart stroke={1.2} />}
+                  fullWidth
+                  className="h-[50px] max-w-[20rem] text-lg font-light"
+                  radius="sm"
+                >
+                  Add To Cart
                 </Button>
               </div>
             </div>
@@ -82,6 +95,16 @@ const ProductPage: PageWithLayout<PageProps> = ({ id }) => {
       )}
     </>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { id } = ctx.query;
+
+  return {
+    props: {
+      id
+    }
+  };
 };
 
 ProductPage.getLayout = function getLayout(page: ReactElement) {
