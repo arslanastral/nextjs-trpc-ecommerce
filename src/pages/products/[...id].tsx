92ee1dee4c073ebd67583e23ffe287d8916ad636
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-11-12 15:39:27 +0800

    removed hooks

--- src/pages/products/[...id].tsx
+++ src/pages/products/[...id].tsx
@@ -3,36 +3,14 @@ import { Product } from '@/lib/types/product';
 import { useRouter } from 'next/router';
 import Image from 'next/image';
 import { Button } from '@mantine/core';
+import { trpc } from '@/utils/trpc';
 import Layout from '@/lib/components/Layouts/Layout';
 
 function Products() {
-  const [product, setProduct] = useState<Product>();
-  const [clickedReadMore, setClickedReadMore] = useState(false);
-  const [orderCount, setOrderCount] = useState(1);
   const router = useRouter();
-  const { id } = router.query;
-  const MAX_CHARACTERS = 135;
-  let isMaxCharacters = false;
-  useEffect(() => {
-    console.log(id);
-    if (id) {
-      fetch(`https://fakestoreapi.com/products/${id}`)
-        .then((res) => res.json())
-        .then((data) => setProduct(data));
-    }
-  }, [id]);
-  function addOrderCount() {
-    setOrderCount((oldCount) => oldCount + 1);
-  }
-  function subractOrderCount() {
-    if (orderCount > 1) {
-      setOrderCount((oldCount) => oldCount - 1);
-    }
-  }
-  if (product?.description && product?.description.length > MAX_CHARACTERS) {
-    isMaxCharacters = true;
-  }
-  console.log(product);
+  const id = (router.query.id as string) ?? '';
+  const productData = trpc.product.sellableProductById.useQuery({ id: id });
+
   return (
     <Layout>
       {product && (
@@ -53,44 +31,20 @@ function Products() {
             </div>
             <h1 className="text-xl font-bold text-zinc-600 max-w-3xl">{product.title}</h1>
             <p className="capitalize text-zinc-500">{product.category}</p>
-            <p className="my-10 order-4 lg:order-5 lg:max-w-3xl">
-              {!isMaxCharacters && <>{product.description}</>}
-              {!clickedReadMore && isMaxCharacters && (
-                <>
-                  {product.description.substring(0, MAX_CHARACTERS) + '...'}{' '}
-                  <button className="text-blue-500" onClick={() => setClickedReadMore(true)}>
-                    Read More
-                  </button>
-                </>
-              )}
-              {clickedReadMore && isMaxCharacters && (
-                <>
-                  {product.description + ' '}
-                  <button className="text-blue-500" onClick={() => setClickedReadMore(false)}>
-                    Read Less
-                  </button>
-                </>
-              )}
-            </p>
+            <p className="my-10 order-4 lg:order-5 lg:max-w-3xl">{productData}</p>
             <div className="mr-1 order-5 md:order-4 py-3">
               <p className="font-extrabold text-4xl text-right mb-5 lg:mb-10 text-zinc-600">
                 $ {product.price}
               </p>
               <div className="flex">
                 <div className="w-[40%] flex">
-                  <div
-                    className="p-3 px-5 md:px-6 bg-zinc-100 rounded-l-lg border border-zinc-200 text-zinc-500"
-                    onClick={subractOrderCount}
-                  >
+                  <div className="p-3 px-5 md:px-6 bg-zinc-100 rounded-l-lg border border-zinc-200 text-zinc-500">
                     -
                   </div>
                   <div className="p-3 px-5 md:px-8 lg:px-10 border border-zinc-200 text-zinc-500">
                     {orderCount}
                   </div>
-                  <div
-                    className="p-3 px-5 md:px-6 bg-zinc-100 rounded-r-lg border border-zinc-200 text-zinc-500"
-                    onClick={addOrderCount}
-                  >
+                  <div className="p-3 px-5 md:px-6 bg-zinc-100 rounded-r-lg border border-zinc-200 text-zinc-500">
                     +
                   </div>
                 </div>
