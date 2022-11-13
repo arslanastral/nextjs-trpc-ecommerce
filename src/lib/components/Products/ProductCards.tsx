cd77b883ed53864a7cd856a866152c71bb0d1bbc
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-11-13 19:32:22 +0800

    added card loading skeleton

--- src/lib/components/Products/ProductCards.tsx
+++ src/lib/components/Products/ProductCards.tsx
@@ -1,23 +1,27 @@
 import { trpc } from '@/utils/trpc';
 import ProductCard from './ProductCard';
+import ProductCardSkeleton from './ProductCardSkeleton';
 
 function ProductCards() {
-  const products = trpc.product.sellableProducts.useQuery();
+  const { data, isLoading, error } = trpc.product.sellableProducts.useQuery();
 
   return (
     <div className="flex flex-wrap justify-center gap-10">
-      {products.data?.map((e, i) => {
-        return (
-          <ProductCard
-            key={i}
-            id={e.id}
-            title={e.title}
-            description={e.description}
-            price={(+e.priceInCents / 100).toString()}
-            image={`https://res.cloudinary.com/dv9wpbflv/image/upload/v${e.image}.jpg`}
-          />
-        );
-      })}
+      {isLoading && Array.from(Array(8).keys()).map((e, i) => <ProductCardSkeleton key={i} />)}
+
+      {data &&
+        data.map((e, i) => {
+          return (
+            <ProductCard
+              key={i}
+              id={e.id}
+              title={e.title}
+              description={e.description}
+              price={(+e.priceInCents / 100).toString()}
+              image={`https://res.cloudinary.com/dv9wpbflv/image/upload/v${e.image}.jpg`}
+            />
+          );
+        })}
     </div>
   );
 }
