4908399ac9c018f4657959faa883f6a707ed7571
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-11-13 23:20:46 +0800

    added procedure for adding to cart

--- src/server/routers/cart.ts
+++ src/server/routers/cart.ts
@@ -1,6 +1,7 @@
 import { router, protectedProcedure } from '../trpc';
 import { getBuyerId, getCartId } from '@/server/functions/identity';
 import { sellerInfoInput } from '../schema';
+import { z } from 'zod';
 
 export const cartRouter = router({
   getCartItems: protectedProcedure.query(async ({ ctx }) => {
@@ -19,11 +20,27 @@ export const cartRouter = router({
     return cartItems;
   }),
 
-  addToCart: protectedProcedure.query(async ({ ctx }) => {
-    let id = await getBuyerId(ctx);
+  addToCart: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ input, ctx }) => {
+      let cartId = await getCartId(ctx);
+      if (!cartId) return null;
 
-    return id;
-  }),
+      let addedItem = await ctx.prisma.cart.update({
+        where: {
+          id: cartId
+        },
+        data: {
+          items: {
+            connect: {
+              id: input.id
+            }
+          }
+        }
+      });
+
+      return addedItem;
+    }),
 
   removeFromCart: protectedProcedure.query(async ({ ctx }) => {
     let id = await getBuyerId(ctx);
