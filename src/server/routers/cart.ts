aaea01c6f2e35f7ee386c714ffba0948649c3b68
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-11-14 17:28:31 +0800

    added procedure to get item count

--- src/server/routers/cart.ts
+++ src/server/routers/cart.ts
@@ -4,6 +4,21 @@ import { sellerInfoInput } from '../schema';
 import { z } from 'zod';
 
 export const cartRouter = router({
+  getItemCount: protectedProcedure.query(async ({ ctx }) => {
+    let cartId = await getCartId(ctx);
+    if (!cartId) return null;
+
+    let items = await ctx.prisma.bag.aggregate({
+      where: {
+        cartId: cartId
+      },
+      _sum: {
+        itemCount: true
+      }
+    });
+
+    return items._sum.itemCount ?? 0;
+  }),
   getCartItems: protectedProcedure.query(async ({ ctx }) => {
     let cartId = await getCartId(ctx);
     if (!cartId) return null;
@@ -13,7 +28,7 @@ export const cartRouter = router({
         id: cartId
       },
       include: {
-        items: true
+        bags: true
       }
     });
 
@@ -26,16 +41,20 @@ export const cartRouter = router({
       let cartId = await getCartId(ctx);
       if (!cartId) return null;
 
-      let addedItem = await ctx.prisma.cart.update({
+      let addedItem = await ctx.prisma.bag.upsert({
         where: {
-          id: cartId
-        },
-        data: {
-          items: {
-            connect: {
-              id: input.id
-            }
+          product_in_cart: {
+            cartId: cartId,
+            productId: input.id
           }
+        },
+        update: {
+          itemCount: { increment: 1 }
+        },
+        create: {
+          cartId: cartId,
+          itemCount: 1,
+          productId: input.id
         }
       });
 
