7d8f392f3cb429aa73861ad9db7efa6586d2a9eb
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-11-15 00:20:19 +0800

    memoize Item row

--- src/lib/components/Cart/Item.tsx
+++ src/lib/components/Cart/Item.tsx
@@ -12,7 +12,7 @@ import { IconTrash } from '@tabler/icons';
 import { ItemInfo } from './ItemInfo';
 import { QuantityInput } from '../Products/QuantityInput';
 import { ItemInfoProps } from './ItemInfo';
-import { useState } from 'react';
+import { memo, useState } from 'react';
 
 const useStyles = createStyles((theme) => ({
   rowSelected: {
@@ -32,35 +32,39 @@ interface ItemRowProps {
 
 type ItemProps = ItemInfoProps & ItemRowProps;
 
-export function Item({ id, toggleRow, title, image, selected, price, quantity, stock }: ItemProps) {
-  const { classes, cx } = useStyles();
-  const [itemQuantity, setItemQuantity] = useState<number | undefined>(1);
+export const Item = memo(
+  ({ id, toggleRow, title, image, selected, price, quantity, stock }: ItemProps) => {
+    const { classes, cx } = useStyles();
+    const [itemQuantity, setItemQuantity] = useState<number | undefined>(1);
 
-  console.log(id);
+    console.log(id);
 
-  return (
-    <tr className={cx({ [classes.rowSelected]: selected })}>
-      <td>
-        <Checkbox checked={selected} onChange={() => toggleRow(id)} transitionDuration={0} />
-      </td>
-      <td>
-        {/* <Group spacing="sm"> */}
-        <ItemInfo image={image} title={title} />
-        {/* </Group> */}
-      </td>
-      <td>
-        <Text size="lg">${price}</Text>
-      </td>
-      <td>
-        <Group spacing={10} position="right">
-          <ActionIcon color="red" m={20} size="lg">
-            <IconTrash size={22} stroke={1.5} />
-          </ActionIcon>
-          <div className="max-w-[130px]">
-            <QuantityInput value={itemQuantity} setValue={setItemQuantity} />
-          </div>
-        </Group>
-      </td>
-    </tr>
-  );
-}
+    return (
+      <tr className={cx({ [classes.rowSelected]: selected })}>
+        <td>
+          <Checkbox checked={selected} onChange={() => toggleRow(id)} transitionDuration={0} />
+        </td>
+        <td>
+          {/* <Group spacing="sm"> */}
+          <ItemInfo image={image} title={title} />
+          {/* </Group> */}
+        </td>
+        <td>
+          <Text size="lg">${price}</Text>
+        </td>
+        <td>
+          <Group spacing={10} position="right">
+            <ActionIcon color="red" m={20} size="lg">
+              <IconTrash size={22} stroke={1.5} />
+            </ActionIcon>
+            <div className="max-w-[130px]">
+              <QuantityInput value={itemQuantity} setValue={setItemQuantity} />
+            </div>
+          </Group>
+        </td>
+      </tr>
+    );
+  }
+);
+
+Item.displayName = 'Item';
