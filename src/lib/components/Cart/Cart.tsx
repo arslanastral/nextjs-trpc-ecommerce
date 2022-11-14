f5e32dafec17522db4d1c258a67cf7c2756fd71e
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-11-14 18:44:52 +0800

    updated styling

--- src/lib/components/Cart/Cart.tsx
+++ src/lib/components/Cart/Cart.tsx
@@ -2,13 +2,15 @@ import { Button, Text, Skeleton, Loader, Title } from '@mantine/core';
 
 function Cart() {
   return (
-    <div className="p-6 ">
-      <Title order={1} color="dark">
-        My Cart
-      </Title>
-      <div className="bg-white mt-8 min-h-[600px] rounded flex gap-6">
-        <div className="bg-slate-100 text-black flex-1">Items</div>
-        <div className="bg-black text-white min-w-[300px]">Checkout</div>
+    <div className="w-full flex justify-center">
+      <div className="min-w-[1400px] max-w-full">
+        <Title order={1} color="dark">
+          My Cart
+        </Title>
+        <div className="mt-8 min-h-[600px] rounded flex gap-6">
+          <div className="bg-white text-white flex-1 rounded-lg">Items</div>
+          <div className="bg-white text-white min-w-[300px]  rounded-lg">Checkout</div>
+        </div>
       </div>
     </div>
   );
