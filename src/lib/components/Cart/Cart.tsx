04cf0268d46ee0edbb3b34cc2d3cc29398c3449e
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-11-14 18:28:22 +0800

    setup initial containers

--- src/lib/components/Cart/Cart.tsx
+++ src/lib/components/Cart/Cart.tsx
@@ -1,5 +1,17 @@
+import { Button, Text, Skeleton, Loader, Title } from '@mantine/core';
+
 function Cart() {
-  return <div>Cart</div>;
+  return (
+    <div className="p-6 ">
+      <Title order={1} color="dark">
+        My Cart
+      </Title>
+      <div className="bg-white mt-8 min-h-[600px] rounded flex gap-6">
+        <div className="bg-slate-100 text-black flex-1">Items</div>
+        <div className="bg-black text-white min-w-[300px]">Checkout</div>
+      </div>
+    </div>
+  );
 }
 
 export default Cart;
