9c3ad4eefae9de727840cadaec3e9998a7f8a2bd
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-11-15 14:01:00 +0800

    fetch cart items from db

--- src/lib/components/Cart/Cart.tsx
+++ src/lib/components/Cart/Cart.tsx
@@ -1,7 +1,16 @@
+import { trpc } from '@/utils/trpc';
 import { Button, Text, Skeleton, Loader, Title, Table } from '@mantine/core';
 import { ItemsSelect } from './ItemsSelect';
+import { useState } from 'react';
 
 function Cart() {
+  const [price, setPrice] = useState<number | undefined>(0.0);
+  const { data, isLoading, error } = trpc.cart.getCartItems.useQuery();
+
+  if (data) {
+    console.log(data);
+  }
+
   return (
     <div className="w-full flex justify-center">
       <div className="w-[max(92%,1400px)] ">
@@ -10,7 +19,7 @@ function Cart() {
         </Title>
         <div className="mt-8 min-h-[600px] rounded flex gap-6 flex-col lg:flex-row">
           <div className="bg-white text-white flex-1 rounded-lg">
-            <ItemsSelect data={data.data} />
+            {data && <ItemsSelect data={data} setPrice={setPrice} />}
           </div>
           <div className="bg-white text-white min-w-[300px] rounded-lg flex flex-col justify-between p-4">
             <div className="text-black w-full  min-h-[300px] ">
@@ -18,7 +27,7 @@ function Cart() {
                 <tbody>
                   <tr>
                     <td className="text-left">Subtotal</td>
-                    <td className="text-right">$0.00</td>
+                    <td className="text-right">${price}</td>
                   </tr>
                   <tr>
                     <td className="text-left">Shipping</td>
@@ -26,7 +35,7 @@ function Cart() {
                   </tr>
                   <tr>
                     <td className="text-left font-semibold">Total</td>
-                    <td className="text-right font-semibold">$0</td>
+                    <td className="text-right font-semibold">${price}</td>
                   </tr>
                 </tbody>
               </Table>
@@ -41,45 +50,5 @@ function Cart() {
     </div>
   );
 }
-
-let data = {
-  data: [
-    {
-      id: 'fsd1',
-      avatar:
-        'https://res.cloudinary.com/dv9wpbflv/image/upload/v1668320696/zavy/naqzoqkkf5ieby7pyros.png',
-      title: 'Mesh Panel Ruched Glitter Top',
-      price: '10.00'
-    },
-    {
-      id: 'sdf2',
-      avatar:
-        'https://res.cloudinary.com/dv9wpbflv/image/upload/v1668320894/zavy/o7hzthnrnhlii05fhovy.png',
-      title: 'Men Cut And Sew Tee',
-      price: '32.22'
-    },
-    {
-      id: '3sdf',
-      avatar:
-        'https://res.cloudinary.com/dv9wpbflv/image/upload/v1668321322/zavy/qvb75enduzln2mebfllx.png',
-      title: 'Mesh Lantern Sleeve Scoop Neck Top',
-      price: '8'
-    },
-    {
-      id: '4sdf',
-      avatar:
-        'https://res.cloudinary.com/dv9wpbflv/image/upload/v1668322182/zavy/pynu2ewt24ghfrmuzaup.png',
-      title: 'Neckline Flounce Sleeve Blouse',
-      price: '10'
-    },
-    {
-      id: '5sdf',
-      avatar:
-        'https://res.cloudinary.com/dv9wpbflv/image/upload/v1668322373/zavy/up6eyghujhgzoce9mfi8.png',
-      title: 'N°5 Eau de Parfum Spray',
-      price: '146'
-    }
-  ]
-};
 
 export default Cart;
