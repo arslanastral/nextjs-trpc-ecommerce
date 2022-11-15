f2d7671b4cdb2fa423db8c2886c59dc75581e912
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-11-15 22:07:13 +0800

    added cart session check

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -4,6 +4,27 @@ import { ReactElement } from 'react';
 import Layout from '@/lib/components/Layouts/Layout';
 import { useSession, signOut } from 'next-auth/react';
 import Cart from '@/lib/components/Cart/Cart';
+import { unstable_getServerSession } from 'next-auth';
+import { authOptions } from '@/pages/api/auth/[...nextauth]';
+import { GetServerSideProps } from 'next';
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { req, res } = ctx;
+  const session = await unstable_getServerSession(req, res, authOptions);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false
+      }
+    };
+  }
+
+  return {
+    props: {}
+  };
+};
 
 const CartPage: PageWithLayout = () => {
   const { data: session, status } = useSession();
