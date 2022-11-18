bb5aa948cd5193ee51989e852048fb4945b417fd
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-11-18 11:40:27 +0800

    init payment while placing order

--- src/server/routers/order.ts
+++ src/server/routers/order.ts
@@ -2,6 +2,7 @@ import { router, protectedProcedure } from '../trpc';
 import { getBuyerId, getCartId } from '@/server/functions/identity';
 import { getSelectedOrderItems, getCartItemsPrice } from '@/server/functions/cart';
 import { z } from 'zod';
+import { stripe } from '@/utils/stripe';
 
 export const orderRouter = router({
   placeOrder: protectedProcedure
@@ -16,13 +17,38 @@ export const orderRouter = router({
       let items = await getSelectedOrderItems(ctx);
       if (!items) return null;
 
-      let price = await getCartItemsPrice(ctx);
+      let lineItems = items.map((e) => e.items).flat();
+
+      const session = await stripe.checkout.sessions.create({
+        line_items: lineItems.map((item) => {
+          return {
+            price_data: {
+              currency: 'usd',
+              product_data: {
+                name: item.title
+              },
+              unit_amount: item.priceInCents
+            },
+            quantity: item.itemCount
+          };
+        }) as [],
+
+        mode: 'payment',
+        success_url: `${process.env.NEXTAUTH_URL}cart`,
+        cancel_url: `${process.env.NEXTAUTH_URL}checkout`
+      });
 
       let sellerOrders = [];
 
+      let payment = await ctx.prisma.payment.create({
+        data: {
+          id: session.id
+        }
+      });
+
       for await (const bag of items) {
         const bagPrice: number = bag.items.reduce((price: number, bag) => {
-          let currentItemPrice: number = (bag.itemCount * +bag.priceInCents) / 100;
+          let currentItemPrice: number = bag.itemCount * +bag.priceInCents * 100;
           return price + currentItemPrice;
         }, 0);
 
@@ -36,13 +62,14 @@ export const orderRouter = router({
                 return { id: item.id };
               })
             },
-            sellerId: bag.sellerId
+            sellerId: bag.sellerId,
+            paymentId: payment.id
           }
         });
 
         sellerOrders.push(order);
       }
 
-      return sellerOrders;
+      return session.url;
     })
 });
