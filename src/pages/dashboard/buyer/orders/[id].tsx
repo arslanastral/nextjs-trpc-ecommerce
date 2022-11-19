357c308ee9095143c64726a101416053ac333306
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-11-19 20:12:44 +0800

    added order page

---
+++ src/pages/dashboard/buyer/orders/[id].tsx
@@ -0,0 +1,35 @@
+import Head from 'next/head';
+import { ReactElement } from 'react';
+import { trpc } from '@/utils/trpc';
+import { DashboardLayout } from '@/lib/components/Layouts/DashboardLayout';
+import NextError from 'next/error';
+import { PageWithLayout } from '@/lib/types/page';
+import { useRouter } from 'next/router';
+
+const BuyerOrderPage: PageWithLayout = () => {
+  const id = useRouter().query.id as string;
+  //   const { data, error, isLoading } = trpc.product.sellableProductById.useQuery(
+  //     { id },
+  //     { enabled: !!id }
+  //   );
+
+  //   if (error) {
+  //     return <NextError title={error.message} statusCode={error.data?.httpStatus ?? 500} />;
+  //   }
+
+  return (
+    <>
+      <Head>
+        <title>Order | Zavy</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <div>Order No. {id}</div>
+    </>
+  );
+};
+
+BuyerOrderPage.getLayout = function getLayout(page: ReactElement) {
+  return <DashboardLayout>{page}</DashboardLayout>;
+};
+
+export default BuyerOrderPage;
