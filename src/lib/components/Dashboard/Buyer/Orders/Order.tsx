003390de4c02b1c35c54d009c56e3c6fa4b2ac84
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-11-19 18:42:58 +0800

    added types

--- src/lib/components/Dashboard/Buyer/Orders/Order.tsx
+++ src/lib/components/Dashboard/Buyer/Orders/Order.tsx
@@ -2,19 +2,37 @@ import { Text, Button, Badge } from '@mantine/core';
 import { ItemInfo } from '@/lib/components/Cart/ItemInfo';
 import { IconBuildingStore, IconFileInvoice, IconBox } from '@tabler/icons';
 
-function Order() {
+type Bag = {
+  item: {
+    title: string;
+    image: string;
+    priceInCents: string;
+  };
+  productId: string;
+  itemCount: number;
+};
+
+type BuyerOrderProps = {
+  bags: Bag[];
+  sellerName: string;
+  paymentStatus: string;
+  orderStatus: string;
+  totalPrice: string;
+};
+
+function Order({ bags, sellerName, paymentStatus, orderStatus, totalPrice }: BuyerOrderProps) {
   return (
     <div className="mt-4 rounded-lg p-4 bg-white">
       <div className="flex items-center justify-between">
         <Button leftIcon={<IconBuildingStore stroke={1.5} />} variant="subtle">
-          {order.seller.storeName}
+          {sellerName}
         </Button>
-        <Badge>{order.payment?.status === 'SUCCESS' ? order.status : order.payment?.status}</Badge>
+        <Badge>{paymentStatus === 'SUCCESS' ? orderStatus : paymentStatus}</Badge>
       </div>
 
-      {order && (
+      {bags && (
         <div className="border rounded-lg mt-4">
-          {order.Bag.map((bag, i) => {
+          {bags.map((bag, i) => {
             return (
               <div key={i} className="p-3">
                 <ItemInfo
@@ -31,8 +49,7 @@ function Order() {
       )}
       <div className="flex justify-between mt-4 items-center">
         <Text className="text-md pl-3 flex gap-2 text-brown-500">
-          <IconFileInvoice stroke={1.5} /> $
-          {order?.totalPriceInCents ? (+order?.totalPriceInCents / 100).toString() : 'NA'}
+          <IconFileInvoice stroke={1.5} /> ${totalPrice}
         </Text>
 
         <Button variant="outline" leftIcon={<IconBox stroke={1.5} size={20} />}>
