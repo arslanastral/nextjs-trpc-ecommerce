954dbbb5fe879e533ac6ad6513ed84400254cd87
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-11-19 23:42:14 +0800

    implemented order timeline

--- src/lib/components/Dashboard/Buyer/Orders/OrderTimeline.tsx
+++ src/lib/components/Dashboard/Buyer/Orders/OrderTimeline.tsx
@@ -4,7 +4,9 @@ import {
   IconPackage,
   IconTruck,
   IconPackgeExport,
-  IconHeartHandshake
+  IconHeartHandshake,
+  IconAlertTriangle,
+  IconCircleX
 } from '@tabler/icons';
 
 type OrderTimelineProps = {
@@ -22,31 +24,88 @@ type OrderTimelineProps = {
 };
 
 export function OrderTimeline({ orderStatus, paymentStatus, sellerName }: OrderTimelineProps) {
+  let status = {
+    OUTOFSTOCK: 0,
+    USERCANCELLED: 0,
+    SELLERCANCELLED: 0,
+    PROCESSING: 0,
+    PACKED: 1,
+    SHIPPED: 2,
+    OUTFORDELIVERY: 3,
+    DELIVERED: 4
+  };
+
+  let processingInfo = {
+    PENDING: 'Awaiting order payment',
+    SUCCESS: `${sellerName ?? 'Seller'} is processing your order`,
+    FAILED: 'Awaiting order payment'
+  };
+
+  let processingIcon = {
+    SUCCESS: <IconLoader size={12} />,
+    PENDING: <IconAlertTriangle size={12} />,
+    FAILED: <IconCircleX size={12} />
+  };
+
+  let processingColor = {
+    SUCCESS: '',
+    PENDING: 'orange',
+    FAILED: 'red'
+  };
+
+  let processingTitle = {
+    SUCCESS: 'Processing',
+    PENDING: 'Pending Payment',
+    FAILED: 'Pending Failed'
+  };
+
   return (
     <div className="bg-white p-10 rounded-lg">
       <Title order={2} weight={300} color="dark">
         Order Tracker
       </Title>
-      <Timeline active={1} bulletSize={24} lineWidth={2} mt={15}>
-        <Timeline.Item bullet={<IconLoader size={12} />} title="Processing">
+      <Timeline
+        active={status[orderStatus]}
+        bulletSize={24}
+        lineWidth={2}
+        mt={15}
+        color={paymentStatus && processingColor[paymentStatus]}
+      >
+        <Timeline.Item
+          bullet={paymentStatus && processingIcon[paymentStatus]}
+          title={paymentStatus && processingTitle[paymentStatus]}
+          lineVariant={status[orderStatus] === 0 ? 'dashed' : 'solid'}
+        >
           <Text color="dimmed" size="sm">
-            Zavy is processing your order
+            {paymentStatus && processingInfo[paymentStatus]}
           </Text>
         </Timeline.Item>
 
-        <Timeline.Item bullet={<IconPackage size={12} />} title="Packed">
+        <Timeline.Item
+          bullet={<IconPackage size={12} />}
+          title="Packed"
+          lineVariant={status[orderStatus] === 1 ? 'dashed' : 'solid'}
+        >
           <Text color="dimmed" size="sm">
             Seller has packed your order
           </Text>
         </Timeline.Item>
 
-        <Timeline.Item title="Shipped" bullet={<IconPackgeExport size={12} />} lineVariant="dashed">
+        <Timeline.Item
+          title="Shipped"
+          bullet={<IconPackgeExport size={12} />}
+          lineVariant={status[orderStatus] === 2 ? 'dashed' : 'solid'}
+        >
           <Text color="dimmed" size="sm">
             Seller has successfully shipped your order
           </Text>
         </Timeline.Item>
 
-        <Timeline.Item title="Out For Delivery" bullet={<IconTruck size={12} />}>
+        <Timeline.Item
+          title="Out For Delivery"
+          bullet={<IconTruck size={12} />}
+          lineVariant={status[orderStatus] === 3 ? 'dashed' : 'solid'}
+        >
           <Text color="dimmed" size="sm">
             Your order is out for delivery
           </Text>
