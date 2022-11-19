3ffb110cce5541ffbabeecccdb1204b5b33c4276
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-11-19 22:05:03 +0800

    added order timeline comp

---
+++ src/lib/components/Dashboard/Buyer/Orders/OrderTimeline.tsx
@@ -0,0 +1,49 @@
+import { Timeline, Text, Title } from '@mantine/core';
+import {
+  IconLoader,
+  IconPackage,
+  IconTruck,
+  IconPackgeExport,
+  IconHeartHandshake
+} from '@tabler/icons';
+
+export function OrderTimeline() {
+  return (
+    <div className="bg-white p-6 rounded-lg">
+      <Title order={2} weight={300} color="dark">
+        Order Tracker
+      </Title>
+      <Timeline active={1} bulletSize={24} lineWidth={2} mt={15}>
+        <Timeline.Item bullet={<IconLoader size={12} />} title="Processing">
+          <Text color="dimmed" size="sm">
+            Zavy is processing your order
+          </Text>
+        </Timeline.Item>
+
+        <Timeline.Item bullet={<IconPackage size={12} />} title="Packed">
+          <Text color="dimmed" size="sm">
+            Seller has packed your order
+          </Text>
+        </Timeline.Item>
+
+        <Timeline.Item title="Shipped" bullet={<IconPackgeExport size={12} />} lineVariant="dashed">
+          <Text color="dimmed" size="sm">
+            Seller has successfully shipped your order
+          </Text>
+        </Timeline.Item>
+
+        <Timeline.Item title="Out For Delivery" bullet={<IconTruck size={12} />}>
+          <Text color="dimmed" size="sm">
+            Your order is out for delivery
+          </Text>
+        </Timeline.Item>
+
+        <Timeline.Item title="Delivered!" bullet={<IconHeartHandshake size={12} />}>
+          <Text color="dimmed" size="sm">
+            Your order has been delivered successfully
+          </Text>
+        </Timeline.Item>
+      </Timeline>
+    </div>
+  );
+}
