a40bfa11617d073ed905fa84af061e34f343ba65
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-11-19 19:23:21 +0800

    added orders

--- src/lib/components/Dashboard/Buyer/Orders/Orders.tsx
+++ src/lib/components/Dashboard/Buyer/Orders/Orders.tsx
@@ -1,11 +1,33 @@
-import { Text, Title } from '@mantine/core';
-import { IconBox } from '@tabler/icons';
+import { Title } from '@mantine/core';
 import { trpc } from '@/utils/trpc';
+import Order from './Order';
+import { RouterOutput } from '@/utils/trpc';
+
+type BuyerOrder = RouterOutput['order']['getBuyerOrders'];
 
 function Orders() {
+  const { data, isLoading, error } = trpc.order.getBuyerOrders.useQuery();
+
+  if (data) {
+    console.log(data);
+  }
+
   return (
     <div className="p-8">
       <Title order={1}>My Orders</Title>
+      {data &&
+        data.map((order, i) => {
+          return (
+            <Order
+              key={i}
+              bags={order.Bag}
+              orderStatus={order.status}
+              paymentStatus={order.payment?.status}
+              sellerName={order.seller.storeName}
+              totalPrice={order.totalPriceInCents ?? ''}
+            />
+          );
+        })}
     </div>
   );
 }
