ff75ef8ff0bcd260f86fbeafaeb7c6a2df5544e7
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-11-20 00:11:03 +0800

    added order address

---
+++ src/lib/components/Dashboard/Buyer/Orders/OrderAddress.tsx
@@ -0,0 +1,40 @@
+import { Title, Card, Group, Badge, Text } from '@mantine/core';
+import { type OrderAddressType } from './Order';
+
+function OrderAddress({ address }: OrderAddressType) {
+  return (
+    <div className="flex flex-col p-10 rounded-lg bg-white">
+      <Title order={2} weight={300} color="dark">
+        Shipping Address
+      </Title>
+      <div className=" rounded-lg mt-4">
+        <div className="w-64">
+          <Card p="lg" radius="md" withBorder>
+            <Group position="apart" mt="md" mb="xs">
+              <Text weight={500}>{address?.city}</Text>
+              {address?.isDefault && <Badge variant="light">default</Badge>}
+            </Group>
+
+            <Text size="sm" color="dimmed">
+              {address?.addressLine1}
+            </Text>
+
+            <Text size="sm" color="dimmed">
+              {address?.postalCode}
+            </Text>
+
+            <Text size="sm" color="dimmed">
+              {address?.city}
+            </Text>
+
+            <Text size="sm" color="dimmed">
+              {address?.country}
+            </Text>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default OrderAddress;
