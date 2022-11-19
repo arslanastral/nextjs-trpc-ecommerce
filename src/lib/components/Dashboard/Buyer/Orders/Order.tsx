5dfc89c99b4c259ff58dfbdf233881b7d4987dad
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-11-19 22:49:43 +0800

    updated props

--- src/lib/components/Dashboard/Buyer/Orders/Order.tsx
+++ src/lib/components/Dashboard/Buyer/Orders/Order.tsx
@@ -1,13 +1,36 @@
-import { RouterOutput } from '@/utils/trpc';
-import { Title } from '@mantine/core';
+import { Title, Badge, Text } from '@mantine/core';
+import { IconHash, IconBuildingStore } from '@tabler/icons';
+import OrderInfo from './OrderInfo';
+import { OrderTimeline } from './OrderTimeline';
+import { type Bag, BuyerOrderProps } from './OrderItem';
 
-type BuyerOrderData = RouterOutput['order']['getBuyerOrderById'];
-
-function Order(data: BuyerOrderData) {
+function Order({
+  id,
+  bags,
+  sellerName,
+  paymentStatus,
+  orderStatus,
+  totalPrice,
+  paymentLink
+}: BuyerOrderProps) {
   return (
     <div className="p-8">
       <Title order={1}>Your Order</Title>
-      <div className="bg-black">Track</div>
+      <div className="flex items-center gap-4 mt-2">
+        <Title order={4} weight={300} color="dimmed" className="flex items-center">
+          <IconHash /> {id}
+        </Title>
+        <Title order={4} weight={300} color="dimmed" className="flex items-center gap-2">
+          <IconBuildingStore stroke={1.5} /> {sellerName}
+        </Title>
+      </div>
+
+      <div className="flex gap-4 mt-10">
+        <div className="bg-white">
+          <OrderInfo data={bags} />
+        </div>
+        <OrderTimeline />
+      </div>
     </div>
   );
 }
