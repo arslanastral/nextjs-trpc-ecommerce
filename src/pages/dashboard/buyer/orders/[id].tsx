8e4b1a45af4657a5b3a910dcc08adaefc32a5f02
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-11-19 20:17:06 +0800

    import order comp

--- src/pages/dashboard/buyer/orders/[id].tsx
+++ src/pages/dashboard/buyer/orders/[id].tsx
@@ -5,6 +5,7 @@ import { DashboardLayout } from '@/lib/components/Layouts/DashboardLayout';
 import NextError from 'next/error';
 import { PageWithLayout } from '@/lib/types/page';
 import { useRouter } from 'next/router';
+import Order from '@/lib/components/Dashboard/Buyer/Orders/Order';
 
 const BuyerOrderPage: PageWithLayout = () => {
   const id = useRouter().query.id as string;
@@ -23,7 +24,7 @@ const BuyerOrderPage: PageWithLayout = () => {
         <title>Order | Zavy</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <div>Order No. {id}</div>
+      <Order />
     </>
   );
 };
