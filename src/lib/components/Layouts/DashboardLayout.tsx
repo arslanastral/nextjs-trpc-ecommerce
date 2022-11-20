a18d36a3b7ed3d630d16095d2865767e6fe140f1
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-11-20 12:37:20 +0800

    updated font size

--- src/lib/components/Layouts/DashboardLayout.tsx
+++ src/lib/components/Layouts/DashboardLayout.tsx
@@ -44,11 +44,11 @@ const useStyles = createStyles((theme, _params, getRef) => {
       display: 'flex',
       alignItems: 'center',
       textDecoration: 'none',
-      fontSize: theme.fontSizes.sm,
+      fontSize: theme.fontSizes.md,
       color: theme.colorScheme === 'dark' ? theme.colors.dark[1] : theme.colors.gray[7],
       padding: `${theme.spacing.xs}px ${theme.spacing.sm}px`,
       borderRadius: theme.radius.sm,
-      fontWeight: 500,
+      fontWeight: 400,
 
       '&:hover': {
         backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
