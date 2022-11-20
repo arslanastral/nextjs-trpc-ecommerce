faa3baa9e4a50f0b36a5c828a1d4f3fed62a1e40
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-11-20 15:56:51 +0800

    imported order status

--- src/lib/components/Dashboard/Seller/Orders/Order.tsx
+++ src/lib/components/Dashboard/Seller/Orders/Order.tsx
@@ -5,19 +5,22 @@ import { type OrderAddressType } from '@/lib/components/Dashboard/Buyer/Orders/O
 import { type SellerOrderProps } from './OrderItem';
 import OrderAddress from '@/lib/components/Dashboard/Buyer/Orders/OrderAddress';
 import { PaymentSummary } from '@/lib/components/Dashboard/Buyer/Orders/PaymentSummary';
+import { OrderStatus } from './OrderStatus';
 
 type OrderPropsWithAddress = SellerOrderProps & OrderAddressType;
 
 function Order({ id, bags, orderStatus, totalPrice, address }: OrderPropsWithAddress) {
   return (
     <div className="p-3">
-      <Title order={1}>Buyer Order</Title>
+      <div className="flex items-center justify-between gap-3 mt-4 flex-wrap">
+        <Title order={1}>Buyer Order</Title> <OrderStatus />
+      </div>
+
       <div className="flex items-center gap-3 mt-4 flex-wrap">
         <Title order={4} weight={300} color="dimmed" className="flex items-center">
           <IconHash /> {id}
         </Title>
       </div>
-
       <div className="flex gap-4 mt-10 flex-col xl:flex-row">
         <OrderInfo data={bags} />
       </div>
