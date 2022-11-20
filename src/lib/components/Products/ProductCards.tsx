a1baea89d083bb9fbe094442a2a45180c912296d
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-11-20 23:11:44 +0800

    add search prop

--- src/lib/components/Products/ProductCards.tsx
+++ src/lib/components/Products/ProductCards.tsx
@@ -4,8 +4,19 @@ import NextError from 'next/error';
 import ProductCard from './ProductCard';
 import ProductCardSkeleton from './ProductCardSkeleton';
 
-function ProductCards({ category, title }: { category?: number | undefined; title?: string }) {
-  const { data, isLoading, error } = trpc.product.sellableProducts.useQuery({ id: category });
+function ProductCards({
+  category,
+  title,
+  searchTerm
+}: {
+  category?: number | undefined;
+  title?: string;
+  searchTerm?: string;
+}) {
+  const { data, isLoading, error } = trpc.product.sellableProducts.useQuery({
+    id: category,
+    searchTerm
+  });
 
   if (error) {
     return <NextError title={error.message} statusCode={error.data?.httpStatus ?? 500} />;
@@ -22,6 +33,26 @@ function ProductCards({ category, title }: { category?: number | undefined; titl
             {data && !data.length && (
               <Title weight={300} order={3} color="dimmed" mt={20}>
                 No Products Added In This Category Yet.
+              </Title>
+            )}
+          </div>
+        </div>
+      )}
+
+      {searchTerm && (
+        <div className="flex w-full justify-center min-h-[6rem]">
+          <div className="flex flex-col items-center mb-4">
+            <Title weight={300} order={1} color="dark" td="underline" className="text-4xl">
+              Search Results
+            </Title>
+            {data && data.length && (
+              <Title weight={300} order={3} color="dimmed" mt={20}>
+                Found {`(${data.length})`} Products Matching Term: {searchTerm}
+              </Title>
+            )}
+            {data && !data.length && (
+              <Title weight={300} order={3} color="dimmed" mt={20}>
+                No Products Found For Term: {searchTerm}
               </Title>
             )}
           </div>
