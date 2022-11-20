9c3d51d4953bd5fa5585429c1d76e82bffabfb91
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-11-20 21:18:24 +0800

    added redirects

--- src/pages/dashboard/buyer/index.tsx
+++ src/pages/dashboard/buyer/index.tsx
@@ -1,26 +1,16 @@
-import Head from 'next/head';
-import { PageWithLayout } from '@/lib/types/page';
-import { ReactElement } from 'react';
-import { DashboardLayout } from '@/lib/components/Layouts/DashboardLayout';
-import { useSession, signOut } from 'next-auth/react';
+import { GetServerSideProps } from 'next';
 
-const BuyerDashboard: PageWithLayout = () => {
-  const { data: session, status } = useSession();
-
-  return (
-    <>
-      <Head>
-        <title>Zavy | Buyer</title>
-        <meta name="description" content="An ecommerce store" />
-      </Head>
-
-      <div>Hello Buyer</div>
-    </>
-  );
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    redirect: {
+      destination: '/dashboard/buyer/orders',
+      permanent: false
+    }
+  };
 };
 
-BuyerDashboard.getLayout = function getLayout(page: ReactElement) {
-  return <DashboardLayout>{page}</DashboardLayout>;
-};
+function Dashboard() {
+  return;
+}
 
-export default BuyerDashboard;
+export default Dashboard;
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,24 +1,9 @@
-// import { unstable_getServerSession } from 'next-auth';
-// import { authOptions } from '@/pages/api/auth/[...nextauth]';
 import { GetServerSideProps } from 'next';
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  // const { req, res } = ctx;
-  // const session = await unstable_getServerSession(req, res, authOptions);
-
-  // if (!session) {
-  //   return {
-  //     redirect: {
-  //       source: '/dashboard/:*',
-  //       destination: '/login',
-  //       permanent: false
-  //     }
-  //   };
-  // }
-
   return {
     redirect: {
-      destination: '/dashboard/buyer',
+      destination: '/dashboard/buyer/orders',
       permanent: false
     }
   };
