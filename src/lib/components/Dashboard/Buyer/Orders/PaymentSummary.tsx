eec6a3434c25a9db8f99005d912ceb227891ab5f
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-11-20 12:31:29 +0800

    fixed payment button

--- src/lib/components/Dashboard/Buyer/Orders/PaymentSummary.tsx
+++ src/lib/components/Dashboard/Buyer/Orders/PaymentSummary.tsx
@@ -11,25 +11,29 @@ export function PaymentSummary({
   paymentLink?: string;
   paymentStatus: 'PENDING' | 'SUCCESS' | 'FAILED' | undefined;
 }) {
+  let paymentButton = false;
+  if (paymentStatus === 'PENDING' || paymentStatus === 'FAILED') {
+    paymentButton = true;
+  }
+
   return (
     <div className="bg-white p-10 rounded-lg lg:min-w-[411px]">
       <Title order={2} weight={300} color="dark" mb={15}>
         Payment
       </Title>
       <PricePreview price={totalPriceInCents} shippingPrice="Free" />
-      {paymentStatus === 'PENDING' ||
-        (paymentStatus === 'FAILED' && (
-          <Button
-            component={Link}
-            href={`/payment/${paymentLink}`}
-            fullWidth
-            radius="md"
-            mt={25}
-            className="h-[45px] font-light text-lg"
-          >
-            Pay Now
-          </Button>
-        ))}
+      {paymentButton && (
+        <Button
+          component={Link}
+          href={`/payment/${paymentLink}`}
+          fullWidth
+          radius="md"
+          mt={25}
+          className="h-[45px] font-light text-lg"
+        >
+          Pay Now
+        </Button>
+      )}
     </div>
   );
 }
